4c7b5aefdca1735da4b33f3fc41922b81779527f
blubrandi <blubrandi0@gmail.com>
2019-09-30 12:01:43 -0600

    Completed guided project

--- SwiftFundI.playground/Contents.swift
+++ SwiftFundI.playground/Contents.swift
@@ -2,7 +2,8 @@
 //: ## Hello, World!
 //: As tradition dictates, any exploration of a new language needs to start with "hello, world"
 
-
+let helloString = "Hello, World!"
+print(helloString)
 
 //: ## Comments
 //: A comment is text that is ignored by the compiler. So it can be plain text, or even code. If it formatted like the examples below, it will be completely ignored when the project is built. Comments are notes to yourself or others about how a given block of code functions.
@@ -17,82 +18,192 @@
 //: ## Identifiers
 //: first character A-Z or a-z or _, followed by any of these plus 0-9
 
-
+let lambdaSchool = "awesome", ls100 = "100%", _travelBack = "1985"
 
 //: ## Mutability
 //: There are two kinds of containers that hold values in Swift. One kind is called a constant, because once a value has been set, it cannot change for the lifetime of that container. Constants are declared with the `let` keyword.
 
+let students = 21
+let staff = 4
+let totalPeople = students + staff
 
 
 //: Variables are declared with the 'var' keyword
 
-
+var appleCount = 10
+appleCount += 15
+print(appleCount)
 
 //: ## Type Inference and Explicit Typing
 //: All containers in Swift must have a type set on them. The containers we've seen so far had their types set implicitly. This is called _type inference_. Meaning the kind of object stored in the container was determined by looking at what kind of value was assigned to the container.
 
-
+let ios = "Apple's mobile operating system"
 
 //: In the line above, the constant `ios` stores a value "Apple's mobile operating system". This value is a `String`, so the data type of `ios` is `String`. The type was _inferred_ by the value provided.
 //: Another way to type a container is to set it explicitly. See some examples of this below.
 
+let price: Double = 1299
+let tax = 53.25
+let total = price + tax
 
+let onSale: Bool = false
+let modelName: String = "MacBook Pro"
+let discountQuantity: Int = 15
 
 //: ## Type Safety
 //: Once a container's data type is set, it cannot hold a value of any other type
 
-
+var life = 42
+//life = "The ultimate answer to life, the universe and everything."
 
 //: ## Type Coercion
 //: Types are never automatically converted into another type.
 
+let label = "The width is "
+let width = 94
 
-
+let widthLabel = label + String(width)
 //: ## String Interpolation and Concatenation
 //: Using the `+` operator with two strings is called _string concatentation_. One string is appended to the end of another.
 
-
+let heightLabel = "The height is " + "100"
 
 //: The below example shows the _string interpolation_ characters (`\()`). These allow a `String` (or non-string) value to be placed in-line inside a larger `String` literal value. If the value is not already a `String`, it will be converted.
 
-
-
+//let anotherWidthLabel = "The width is \(width)"
+let anotherWidthLabel = "The width is \(width + 5)"
 //: ## Collections
 //: ### Arrays
 //: A Swift array is a collection of like objects, and they are stored in the order they are added. Accessing a particular element is done by using its index value (which starts at 0).
 
-
-
+var shipCaptains = ["Malcolm Reynolds", "Jean Luc Picard", "James T Kirk", "Han Solo"]
+print(shipCaptains[1])
+let sameCaptains = ["Malcolm Reynolds", "Jean Luc Picard", "James T Kirk", "Han Solo"]
+let differentCaptains = ["Kathryn Janeway", "Poe Dameron"]
 //: Array equality is determined by looking at each element. If all elements are the same, and in the same order, the two arrays are equal.
-
+if shipCaptains == differentCaptains {
+    print("Arrays are equal")
+} else {
+    print("Arrays are not equal")
+}
 
 
 //: Since `shipCaptains` was declared as a constant, we must convert it to a mutable array before we can add entries.
+var moreCaptains = shipCaptains
+moreCaptains.append("Katheryn Janeway")
+print(moreCaptains)
+moreCaptains[1] = "Poe Dameron"
+// ^^ Replaces the value at that index.  Use insert to add, instead of replace.
+print(moreCaptains)
 
-
-
+moreCaptains.insert("Jean Luc Picard", at: 1)
 //: ### Dictionaries
 //: A Swift dictionary is similar to an array in that is a collection, but the items are stored in no particular order. To access an item, its associated key is provided. Dictionaries are also called "key-value" stores. The key has a type, and the value can have the same or a different type.
 
+var occupations = ["Malcom": "Captain", "Kaylee": "Mechanic"]
+occupations["Malcom"]
+
+// Since dictionaries are unordered, when a change is added, the key/value pair does not overwrite the existing value at that index
+
+occupations["Jayne"] = "Public Relations"
+print(occupations)
 
 
 //: ## Looping
 //: ### `for`
 //: `for` loops allow for iterating over a collection of elements (usually an array), examining each element in turn.
 
+let scores = [75, 43, 103, 87, 12]
+var passingStudents = 0
 
+for score in scores {
+    if score >= 70 {
+        passingStudents += 1
+    }
+}
 
+print("\(passingStudents) students scored a passing grade of 70 or higher.")
 //: ### `switch`
 //: `switch` statements are useful when a multiple-choice path is possible for your code, and you want to choose a single path at runtime, based on some criteria.
 
+let furniture = "Chair"
 
+switch furniture {
+case "Table":
+    print("Got a table.")
+case "Chair", "Stool", "Bench":
+    print("Got something to sit on.")
+default:
+    print("Default.")
+}
+
+let number = 6
+switch number
+{
+case 1:
+    print("Got 1")
+case 2:
+    print("Got 2")
+case 3, 4, 5:
+    print("Got 3 or 4 or 5")
+case 6...22:
+    print("Got 6 to 22")
+default:
+    print("Got Default")
+}
 
 //: ## Functions
 //: A function in Swift is nothing more than a collection of instructions that accomplish some task. The advantage to grouping the instructions into a function is that the function can then be called whenever that task should be performed, rather than having to list out those instrutions a second, third, or nth time.
 
+func averageScore(_ scores: [Int]) -> Int {
+    var totalScore = 0
+    
+    for score in scores {
+        totalScore += score
+    }
+    return totalScore / scores.count
+}
 
+let avg = averageScore(scores)
 
+// ^^^^ Keep in mind in this example, that the result is returned as an Interger, so the result might not be correct because the floating points are truncated.
+
+func averageScoreHighPrecision(_ scores: [Int]) -> Double {
+    var totalScore: Double = 0
+    
+    for score in scores {
+        totalScore += Double(score)
+    }
+    return totalScore / Double(scores.count)
+}
+
+let avgHP = averageScoreHighPrecision(scores)
 //: ## Tuples
 //: Tuples are a lightweight way of grouping related values into a single compound value.
 
+let carrots = (name: "carrots", aisle: 4, category: "produce", count: 25)
+let soup = (name: "tomato soup", aisle: 6, category: "canned goods", count: 100)
+let cereal = (name: "Frosted Flakes", aisle: 2, category: "cereals", count: 43)
+let pringles = (name: "Pringles", aisle: 7, category: "chips", count: 15)
+let tostitos = (name: "Tostitos", aisle: 7, category: "chips", count: 26)
+let seafood = (name: "crab legs", aisle: 1, category: "seafood", count: 8)
+let rice = (name: "Minute Rice", aisle: 2, category: "dry rice and beans", count: 62)
+let turkey = (name: "Boar's Head turkey", aisle: 9, category: "deli", count: 4)
 
+let groceries = [carrots, soup, cereal, pringles, tostitos, seafood, rice, turkey]
+
+print(groceries)
+
+// Get a total count of all the items in the store
+
+func determineTotalCount() -> Int {
+    var totalCount = 0
+    
+    for item in groceries {
+        totalCount += item.count
+    }
+    
+    return totalCount
+}
+
+print("Total count of all grocery items: \(determineTotalCount())")
